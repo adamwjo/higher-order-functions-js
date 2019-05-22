349cce3961e8e736c0111a513a68d111f370a621
adamwjo <adamwjo@gmail.com>
2019-05-21 21:56:26 -0400

    set up shell

---
+++ src.js
@@ -0,0 +1,54 @@
+
+const avengers = [
+    {
+        name: "Iron-Man",
+        powerLevel: 500,
+        talent: "tech",
+        isDead: false
+    },
+    {
+        name: "Black-Widow",
+        powerLevel: 200,
+        talent: "stealth",
+        isDead: false
+    },
+    {
+        name: "Black-Panther",
+        powerLevel: 423,
+        talent: "stealth",
+        isDead: true
+    },
+    {
+        name: "Spider-Man",
+        powerLevel: 700,
+        talent: "tech",
+        isDead: true
+    },
+    {
+        name: "Thor",
+        powerLevel: 1000,
+        talent: "strength",
+        isDead: false
+    },
+    {
+        name: "hulk",
+        powerLevel: 1200,
+        talent: "tech",
+        isDead: false
+    }
+];
+
+const infinitySnap = function(avenger) {
+    if(avenger.isDead == "true"){
+        avenger.isDead = "false"
+    }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("were in")
+    let button = document.getElementById('avengers-button')
+    button.addEventListener('click', () => {
+        avengers.forEach(avengers, infinitySnap)
+    });
+    
+})
